80de8322d0ab8c28f5dfbe3cde8759a1efbe06c9
Codrin Ciubotariu <codrin.ciubotariu@freescale.com>
2015-06-05 11:35:34 +0300

    Fix Copyright issues
    The new created files have code taken from uio_seville_main.c and
    we should add all the licenses from the old file into the new ones.
    
    Signed-off-by: Codrin Ciubotariu <codrin.ciubotariu@freescale.com>
    Change-Id: I1efc232cc53b3613420d1f9864f5ae79f14a053b
    Reviewed-on: http://git.am.freescale.net:8181/37454
    Reviewed-by: Cristian-Constantin Sovaiala <Cristian.Sovaiala@freescale.com>
    Reviewed-by: Razvan Stefanescu <razvan.stefanescu@freescale.com>
    Tested-by: Razvan Stefanescu <razvan.stefanescu@freescale.com>

--- uio-driver/phy_seville.c
+++ uio-driver/phy_seville.c
@@ -1,7 +1,8 @@
 /*
  * Seville user-space PHY driver.
  *
- * Copyright (c) 2015 Freescale Semiconductor, Inc.
+ * Copyright (C) 2014 Vitesse Semiconductor Inc.
+ * Copyright (c) 2014-2015 Freescale Semiconductor, Inc.
  *
  * This program is free software; you can redistribute  it and/or modify it
  * under  the terms of  the GNU General  Public License as published by the
